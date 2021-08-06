9a9214d1b3537e4cfd640eceb979fbd4e95dce2f
chike <chike@ziyihuoyun.com>
2021-08-06 08:53:53 +0800

    优化分享逻辑

--- 领克签到.js
+++ 领克签到.js
@@ -3,13 +3,17 @@ function wechatShare() {
     sleep(1000)
     click(280, 2100)
     tools.specialClickRetry("android.widget.LinearLayout", 8, 2, 1, 10)
-    tools.specialRetry("android.widget.RelativeLayout", 8, 4, 3, 10)
-    tools.normalRetry(text, "分享", 2, 10)
-    tools.normalRetry(text, "返回LynkCo", 0, 10)
-    swipe(270, 200, 810, 200, 200)
+    // tools.specialRetry("android.widget.RelativeLayout", 10, 4, 3, 10)
+    // click(600, 660)
+    // tools.normalRetry(text, "分享", 2, 10)
+    // tools.normalRetry(text, "返回LynkCo", 0, 10)
+    // swipe(270, 200, 810, 200, 200)
+    // 不需要真实分享，所以微信页面也需要一次返回
     sleep(1000)
     back()
-    sleep(2000)
+    sleep(1000)
+    back()
+    sleep(1000)
 }
 
 var kill = require("./kill.js")
@@ -25,23 +29,27 @@ for (let i=0; i<2; i++) {
     i == 0
         ? tools.specialClickRetry("android.widget.LinearLayout", 8, 1, 0, 10)
         : tools.specialClickRetry("android.widget.LinearLayout", 8, 2, 1, 10)
-    sleep(1000)
+    sleep(5000)
+    if (text("暂不更新").exists()) {
+        tools.normalRetry(text, "暂不更新", 5, 10)
+        sleep(1000)
+    }
     // 点击动态
     for (let i=0; i<5; i++) {
         tools.specialClickRetry("android.widget.FrameLayout", 17, 2, 1, 20)
     }
     // 点击分享
     click(540, 365)
-    sleep(3000)
+    sleep(5000)
     click(980, 2250)
     wechatShare()
     // 点击活动
     for (let i=0; i<5; i++) {
         tools.specialClickRetry("android.widget.FrameLayout", 17, 3, 2, 10)
     }
-    sleep(3000)
+    sleep(5000)
     click(540, 1200)
-    sleep(3000)
+    sleep(5000)
     // 点击分享
     click(970, 140)
     wechatShare()
@@ -49,11 +57,11 @@ for (let i=0; i<2; i++) {
     for (let i=0; i<5; i++) {
         tools.specialClickRetry("android.widget.FrameLayout", 17, 4, 3, 10)
     }
-    sleep(3000)
+    sleep(5000)
     click(540, 450)
-    sleep(3000)
+    sleep(5000)
     click(990, 2270)
-    sleep(3000)
+    sleep(5000)
     // 点击分享
     click(980, 2280)
     wechatShare()
